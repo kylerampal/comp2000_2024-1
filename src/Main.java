f0b1d2f2695662c729682f12851f628c1829a32d
kylerampal <79913444+kylerampal@users.noreply.github.com>
2024-08-02 13:24:44 +1000

    aa
    aa

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,38 @@
-public class Main {
+import javax.swing.*;
+import java.awt.*;
+
+
+public class Main extends JFrame{
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello World");
+      Main window = new Main();  
+    }
+
+
+    class Canvas extends JPanel {
+      public Canvas() {
+        setPreferredSize(new Dimension(720,720));
+      }
+
+      @Override
+      public void paint(Graphics g) {
+        for(int x = 10; x < 710; x+=35) {
+            for (int y = 10; y < 710; y+=35){
+            g.setColor(Color.WHITE);
+             g.fillRect(x,y,35,35);
+             g.setColor(Color.BLACK);
+             g.drawRect(x,y,35,35);
+     }
+    }
+      }
+    }
+
+
+
+    private Main() {
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setVisible(true);
+        Canvas canvas = new Canvas();
+        this.setContentPane(canvas);
+        this.pack();
     }
 }
