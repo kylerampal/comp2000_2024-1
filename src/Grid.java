1329955a6957c0e37c5dba47e638af6020705b91
kylerampal <79913444+kylerampal@users.noreply.github.com>
2024-08-13 19:18:01 +1000

    part one almost done!!!!! WOOOO
    WOOOOOO

--- src/Grid.java
+++ src/Grid.java
@@ -1,45 +1,37 @@
+
 import java.awt.Graphics;
 import java.awt.Point;
+import java.util.ArrayList;
 
 public class Grid {
-  // fields
-  Cell[][] cells = new Cell[20][20];
-  MouseTrail[] trails = new MouseTrail[100];
-  int mouseTrailIDX = 0;
-  
-  // constructors
-  public Grid() {
-    for(int i=0; i<cells.length; i++) {
-      for(int j=0; j<cells[i].length; j++) {
-        cells[i][j] = new Cell(10+Cell.size*i, 10+Cell.size*j);
-      }
+    // fields
+
+    Cell[][] cells = new Cell[20][20];
+    ArrayList<MouseTrail> trails = new ArrayList<MouseTrail>();
+
+    // constructors
+    public Grid() {
+        for (int i = 0; i < cells.length; i++) {
+            for (int j = 0; j < cells[i].length; j++) {
+                cells[i][j] = new Cell(10 + Cell.size * i, 10 + Cell.size * j);
+            }
+        }
     }
-  }
-  // methods
-  public void paint(Graphics g, Point mousePos) {
+    // methods
 
-// mousetrail_idx++;
-// id mousetrail_idx > 99 {
-//   moustrail_idx = 0;
-// trails[idx],clear_circle = new Moustrail(x,y);
-//}
-
-      
-      trails[mouseTrailIDX] = new MouseTrail(mousePos.x, mousePos.y);
-      mouseTrailIDX++;
-      // System.out.println(mouseTrailIDX);
-      // System.out.println(trails[mouseTrailIDX].x + "x");
-      // System.out.println(trails[mouseTrailIDX].y + "y");
-      if (mouseTrailIDX > 99) {
-        mouseTrailIDX = 0;
-      }
-   
-
-    for(int i=0; i<cells.length; i++) {
-      for(int j=0; j<cells[i].length; j++) {
-        cells[i][j].paint(g, mousePos);
-      }
+    public void paint(Graphics g, Point mousePos) {
+        for (int i = 0; i < cells.length; i++) {
+            for (int j = 0; j < cells[i].length; j++) {
+                cells[i][j].paint(g, mousePos);
+            }
+        }
+        
+            if (trails.size() >= 100) {
+              trails.removeFirst();
+            }
+            trails.add(new MouseTrail(mousePos.x,mousePos.y));
+            for (int i = 0; i < trails.size(); i++) {
+              trails.get(i).paint(g, mousePos);
+            }
     }
-
-  }
 }
--- src/MouseTrail.java
+++ src/MouseTrail.java
@@ -1,9 +1,9 @@
-import java.awt.Point;
+
 import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.Point;
 
 public class MouseTrail {
-
 
     // fields
     int x;
@@ -11,29 +11,26 @@ public class MouseTrail {
     static int size = 10;
 
     // constructor
-
     public MouseTrail(int xPos, int yPos) {
         x = xPos;
         y = yPos;
-        
+
     }
-
-
 
     // methods
     public void paint(Graphics g, Point mousePos) {
         if (contains(mousePos)) {
-            g.setColor(Color.YELLOW);
-            g.drawOval(x,y,size,size);
-            g.fillOval(x,y,size,size);
+          g.setColor(Color.YELLOW);
+          g.drawOval(x, y, size, size);
+          g.fillOval(x, y, size, size);
         }
     }
 
     public boolean contains(Point p) { // check if within grid
-        if(p != null) { 
-          return p.x >= 10 && p.y >= 10 && p.x <= 710 && p.y <= 710;
+        if (p != null) {
+            return p.x >= 10 && p.y >= 10 && p.x <= 710 && p.y <= 710;
         } else {
-          return false;
+            return false;
         }
-      }
+    }
 }
